df3c7051ca7f01511b6043e2b83c1eb55f7cfdc9
voiceinthedark <darkrisingforce@gmail.com>
2025-07-14 10:42:16 +0300

    restaurant-page: Main styling for menu page
    - Added styling for the menu tab
    - Added functionality to create menu page dynamically
    - Added free assets for the menu page

---
+++ README.md
@@ -0,0 +1,5 @@
+# Odin Restaurant Page (dynamic content)
+
+## Assets used
+
+- [foodiesfeed](https://www.foodiesfeed.com/)
---
+++ src/content-menu.js
@@ -0,0 +1,37 @@
+function createContentMenu() {
+  const menuPage = document.createElement('article');
+  menuPage.classList.add('menu-page');
+}
+
+function createMenuItem(name, description, price, imageUrl){
+  const menuItem = document.createElement('div');
+  const itemName = document.createElement('h3');
+  const itemDescription = document.createElement('p');
+  const itemPrice = document.createElement('span');
+  const itemImage = document.createElement('img');
+
+  if (imageUrl){
+    itemImage.src = imageUrl;
+    itemImage.alt = `${name} image`;
+    itemImage.classList.add('item-image');
+  }
+
+  const itemContainer = document.createElement('div');
+  itemContainer.classList.add('item-container');
+  itemContainer.appendChild(itemName);
+  itemContainer.appendChild(itemDescription);
+  itemContainer.appendChild(itemImage);
+
+  itemName.textContent = name;
+  itemDescription.textContent = description;
+  itemDescription.classList.add('item-description');
+  itemPrice.textContent = `$${price.toFixed(2)}`;
+  itemPrice.classList.add('item-price');
+
+  menuItem.appendChild(itemPrice);
+  menuItem.appendChild(itemContainer);
+
+  menuItem.classList.add('item-card');
+  
+  return menuItem;
+}
