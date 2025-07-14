46981aa72da84d3f604368119b3e875d9a6d13e7
voiceinthedark <darkrisingforce@gmail.com>
2025-07-14 15:50:31 +0300

    restaurant page: add more menu items
    - Added more menu items
    - fixed images having same filename

--- src/content-menu.js
+++ src/content-menu.js
@@ -1,5 +1,5 @@
 import imgPancake from './assets/imgs/foodiesfeed.com_pouring-honey-on-pancakesk.jpg';
-import imgBurger from './assets/imgs/462860.jpg';
+import imgBurger from './assets/imgs/burger.jpg';
 import imgCheeseCake from './assets/imgs/foodiesfeed.com_refreshing-lemon-cheesecake-slice-with-mint-garnish.jpg';
 import imgEggsFried from './assets/imgs/foodiesfeed.com_colorful-veggie-bowl-with-eggs-and-fresh-corn.png';
 
@@ -11,12 +11,21 @@ function createContentMenu() {
   menuTitle.textContent = 'Menu';
   menuTitle.classList.add('menu-title');
   menuPage.appendChild(menuTitle);
+  
+  
 
   // create a menu item
   const menuItems = [
     createMenuItem('Spicy Cheese Burger, House Flavor',
     'A delicious burger with a spicy cheese patty, topped with fresh lettuce, tomatoes and our special sauce.',
     8.99, imgBurger),
+    createMenuItem('Honeyed Pancake with blueberries', 
+    'A tasty pancake with honeyed toppings and blueberries on top, creamy on the inside',
+    13.75, imgPancake),
+    createMenuItem('House Cheese Cake!', 'Cheese Odin flavor, creamy and tasty inside out',
+    7.55, imgCheeseCake),
+    createMenuItem('Odin\'s Veggies fried Eggs, house roll', 'Breakfast special, healthy and delicious.',
+    5.89, imgEggsFried),
   ];
 
   for (const item of menuItems){
