c3c7b303f59d5e44881e90f4e9a92ba9a2545326
voiceinthedark <darkrisingforce@gmail.com>
2025-07-14 09:33:52 +0300

    restaurant-page: make content dynamic
    - Removed static content on html
    - Added content dynamically in the script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './assets/462860.jpg'
 import './styles.css'
-console.log('Hello, world!');
+
+const rootContent = document.querySelector('#content');
+
+// Create a header element
+const headerElement = document.createElement('header');
+headerElement.classList.add('content-header');
+
+// Create main title
+const mainTitle = document.createElement('h1');
+mainTitle.textContent = 'Odin Restaurant';
+
+headerElement.appendChild(mainTitle);
+
+rootContent.appendChild(headerElement)
