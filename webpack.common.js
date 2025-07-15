1fe83b745f1c845c69a8f72b968c5a4f2051fe27
voiceinthedark <darkrisingforce@gmail.com>
2025-07-15 19:04:44 +0300

    Refactor webpack and split into modes

--- webpack.common.js
+++ webpack.common.js
@@ -9,10 +9,6 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
 	},
-	devtool: 'eval-source-map',
-	devServer: {
-		watchFiles: ['./src/template.html',],
-	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/template.html',
